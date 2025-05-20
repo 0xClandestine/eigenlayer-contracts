ffbf1c0d118b76ab78531a777018c2e427af0574
clandestine.eth <96172957+0xClandestine@users.noreply.github.com>
2025-05-20 16:29:28 -0400

    fix: gap

--- src/contracts/core/SlashingWithdrawalRouterStorage.sol
+++ src/contracts/core/SlashingWithdrawalRouterStorage.sol
@@ -73,5 +73,5 @@ abstract contract SlashingWithdrawalRouterStorage is ISlashingWithdrawalRouter {
      * variables without shifting down storage in the inheritance chain.
      * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
      */
-    uint256[45] private __gap;
+    uint256[43] private __gap;
 }
